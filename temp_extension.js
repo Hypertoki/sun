c0eef806b989350e44d9563003fbe0cc5039ac88
Hypertoki <33825758+Hypertoki@users.noreply.github.com>
2017-11-30 23:13:58 +0900

    Add files via upload

--- temp_extension.js
+++ temp_extension.js
@@ -33,6 +33,11 @@ var minute = '0';
 var lat = '35.4378';
 var lon = '136.7520';
 
+var moonazi='';
+var moonalt='';
+var age='';
+
+
 var descriptor = {blocks: [
       ['R', '日時を %n 年 %n 月 %n 日 %n 時 %n 分にする', 'set_daytime',2017,6,21,12,0],
       ['R', '場所を %m.point に変更する','set_point','岐阜']
@@ -111,7 +116,8 @@ ext.set_point = function(p, callback){
 
 
   var descriptor = {blocks: [
-      ['R', '太陽の取得', 'get_weather']
+      ['R', '太陽の取得', 'get_weather'],
+      ['R', '月の取得', 'get_luna']      
       ]};
   // 利用する機能名、ブロックの情報、そしてブロックのロジックを含むオブジェクトを登録
   //ScratchExtensions.register('太陽の位置拡張', descriptor, ext);
@@ -194,14 +200,90 @@ ext.get_altitude = function(callback) {
 };
 
 
+// **********************************************************************************************************************************************************
+//          　　　　　　　　　　　　　ここから月の処理
+// **********************************************************************************************************************************************************
+ext.get_weather = function(callback) {
+  $.ajax({
+//    url: 'http://mgpn2.sakura.ne.jp/api/moon/position.cgi?jsonp=caller&y=2000&m=5&d=5&h=12&lat=35.6544&lon=139.7447',
+    url: 'http://mgpn2.sakura.ne.jp/api/moon/position.cgi?jsonp=caller&y='+year+'&m='+month+'&d='+day+'&h='+house+'&min='+minute+'&lat='+lat+'&lon='+lon,
+    type: 'GET',
+    dataType: 'jsonp',
+    jsonpCallback: 'caller',
+//    jsonp : "callbackChange",
+    error: function(XMLHttpRequest, textStatus, errorThrown) {
+    alert("XMLHttpRequest : " + XMLHttpRequest.status);
+    alert("textStatus : " + textStatus);
+    alert("errorThrown : " + errorThrown);
+    callback('');
+    },
+    success: function(data) {
+      if (data.status == 200) {
+//        console.log(data);
+          moonazi = data.result.azimuth;
+          moonalt = data.result.altitude;
+          ago = data.result.age
+//          var Array = [data.result.sunset,data.result.sunrise];
+//          if (main == undefined) {
+//            callback('');
+//          } else {
+            callback('OK');
+//          }
+      } else {
+        callback('');
+      }
+// alert(data.result.sunset);
+    }
+  });
+};
+
+
+ext.get_age = function(callback) {
+	if (sunrise=='') {
+	  callback('0');
+	}else{
+	  callback(age);
+	}
+
+};
+
+ext.get_moonazi = function(callback) {
+	if (azimuth=='') {
+	  callback('0');
+	}else{
+	  callback(moonazi);
+	}
+
+};
+
+
+ext.get_moonalt = function(callback) {
+	if (altitude=='') {
+	  callback('0');
+	}else{
+	  callback(moonalt);
+	}
+
+};
+
+
+
+// **********************************************************************************************************************************************************
+//          　　　　　　　　　　　　　おわり
+// **********************************************************************************************************************************************************
+
+
   // Scratch に作ったブロックを登録します
-  ScratchExtensions.register('お天気拡張', descriptor, ext);
+  ScratchExtensions.register('お空の拡張', descriptor, ext);
 
   var descriptor = {blocks: [
       ['R', '日の出', 'get_sunset'],
       ['R', '日の入', 'get_sunrise'],
       ['R', '方位角', 'get_azimuth'],
-      ['R', '高度', 'get_altitude']
+      ['R', '高度', 'get_altitude'],
+      ['R', '月齢', 'get_age'],
+      ['R', '方位角（月）', 'get_miinazi'],
+      ['R', '高度（月）', 'get_moonalt']
       ]};
   // 利用する機能名、ブロックの情報、そしてブロックのロジックを含むオブジェクトを登録
   //ScratchExtensions.register('太陽の位置拡張', descriptor, ext);
@@ -209,7 +291,7 @@ ext.get_altitude = function(callback) {
 
 
   // Scratch に作ったブロックを登録します
-  ScratchExtensions.register('お天気拡張2', descriptor, ext);
+  ScratchExtensions.register('お空の拡張2', descriptor, ext);
 
 
 
